6bce032b3a045cd5205e526dafcfe942d94b36e6
Jeff Stevenson <jwstevenson22@gmail.com>
2025-04-26 03:16:30 -0400

    merge fixes

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -15,7 +15,7 @@ public class MenuManager : MonoBehaviour
 {
     // name of main menu scene
     private const string MAIN_MENU_STR_REF = "MainMenu";
-    private const string GAME_START_SCENE = "clonescene_withCurrent";
+    private const string GAME_START_SCENE = "GameplayDemoScene";
 
     public static bool IsGamePaused;
 
